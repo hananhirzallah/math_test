354240614b17960bd95196db23a2ff45bf202a74
hananhirzallah <hananhirzallah3@gmail.com>
2024-05-19 14:54:38 +0300

    Hanan_Hirzallah_Task.py

--- Hanan_Hirzallah_Task.py
+++ Hanan_Hirzallah_Task.py
@@ -16,7 +16,7 @@ st.markdown("""
         padding: 10px 20px;
         font-size: 16px;
         cursor: pointer;
-        width: 140px;  /* Adjusted width */
+        width: 120px;  /* Adjusted width */
         height: 40px;  /* Adjusted height */
     }
     .stButton>button:hover {
@@ -55,6 +55,10 @@ st.markdown("""
     }
     .hint {
         color: #007BFF;
+    }
+    input[type="number"] {
+        background-color: #ffffff;  /* White background color */
+        color: #007BFF;  /* Blue text color */
     }
     </style>
 """, unsafe_allow_html=True)
@@ -168,14 +172,6 @@ def main():
     if st.session_state.num_questions is None:
         st.markdown('<p class="blue-text">Choose number of questions (10-20):</p>', unsafe_allow_html=True)
         num_questions = st.number_input('', min_value=10, max_value=20, step=1, key='num_questions_input')
-        # st.markdown("""
-        #     <style>
-        #     div[data-baseweb="input"] > div {
-        #         background-color: #ffffff;  /* White background color */
-        #         font_color: #007BFF;
-        #     }
-        #     </style>
-        # """, unsafe_allow_html=True)
         if st.button('Confirm'):
             st.session_state.num_questions = num_questions
             st.experimental_rerun()
