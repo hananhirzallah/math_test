7489995b0773d09cd1b0a14202a853578583b258
hananhirzallah <hananhirzallah3@gmail.com>
2024-05-19 16:11:21 +0300

    Hanan_Hirzallah_Task.py

--- Hanan_Hirzallah_Task.py
+++ Hanan_Hirzallah_Task.py
@@ -250,6 +250,7 @@ def main():
                     st.session_state.second_chance = False  # Reset second chance flag
                 else:
                     st.session_state.feedback = "Wrong Answer! Try another question of the same difficulty."
+                    st.session_state.question_number += 1  # Increment the question number for the second attempt
                     st.session_state.current_question = generate_arithmetic_question(st.session_state.current_difficulty)
                     st.session_state.start_time = time.time()
                     st.session_state.user_answer = ""  # Reset user answer for the new question
