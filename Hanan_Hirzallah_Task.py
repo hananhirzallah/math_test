aeb7ea17f814a1a44d34aafa2ed1acdd6baa0806
hananhirzallah <hananhirzallah3@gmail.com>
2024-05-19 14:34:26 +0300

    Hanan_Hirzallah_Task.py

--- Hanan_Hirzallah_Task.py
+++ Hanan_Hirzallah_Task.py
@@ -172,6 +172,7 @@ def main():
             <style>
             div[data-baseweb="input"] > div {
                 background-color: #ffffff;  /* White background color */
+                color: #007BFF;
             }
             </style>
         """, unsafe_allow_html=True)
